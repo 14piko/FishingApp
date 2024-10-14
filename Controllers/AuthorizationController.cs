f5053978012cc16ea011437a0151d0ac29468cd9
Mirko Ereš <mirko.eres1@gmail.com>
2024-10-14 23:46:09 +0200

    Authorization backend

---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,58 @@
+﻿using FishingApp.Data;
+using FishingApp.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace FishingApp.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly FishingAppContext _context;
+
+        public AuthorizationController(FishingAppContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("token")]
+        public IActionResult GenerateToken([FromBody] LoginDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userBase = _context.User
+                   .Where(u => u.Email!.Equals(loginDTO.Email))
+                   .FirstOrDefault();
+
+            if (userBase == null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, "Wrong authorization data!");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(loginDTO.Password, userBase.Password))
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, "Oops! The password you entered is incorrect. Please try again.");
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.UTF8.GetBytes("MojKljucKojijeJakoTajan i dovoljno dugačak da se može koristiti");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.Add(TimeSpan.FromHours(8)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwt = tokenHandler.WriteToken(token);
+
+            return Ok(jwt);
+        }
+    }
+}
